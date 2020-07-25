4369554f166c9e41e4e3e4e9a45fed4f41e3fc44
Gio <sethg@ipi.org>
2020-07-24 23:33:33 -0500

    Initial commit

---
+++ html5video.py
@@ -0,0 +1,74 @@
+from markdown.extensions import Extension
+from markdown.inlinepatterns import LinkInlineProcessor
+import xml.etree.ElementTree as etree
+import os
+
+
+# ![alttxt](http://x.com/) or ![alttxt](<http://x.com/>)
+IMAGE_LINK_RE = r'\!\['
+
+# MP4 video/mp4
+# WebM    video/webm
+# Ogg video/ogg
+
+
+VIDEO_EXTENSIONS = {".mp4", ".webm", ".ogg"}
+
+
+class VideoInlineProcessor(LinkInlineProcessor):
+    """ Return a img element from the given match. """
+
+    def handleMatch(self, m, data):
+        text, index, handled = self.getText(data, m.end(0))
+        if not handled:
+            return None, None, None
+
+        src, title, index, mtype, handled = self.getLink(data, index)
+        if not handled:
+            return None, None, None
+
+        el = etree.Element("video")
+
+        el.set("src", src)
+        el.set("type", mtype)
+        el.set("controls", "true")
+
+        if title is not None:
+            el.set("title", title)
+
+        el.set('alt', self.unescape(text))
+        return el, m.start(0), index
+
+    def getLink(self, data, index):
+        href, title, index, handled = super().getLink(data, index)
+        if handled:
+            linkext = os.path.splitext(href)[1].lower()
+            if linkext in VIDEO_EXTENSIONS:
+                mtype = f"video/{linkext.replace('.', '')}"
+                return href, title, index, mtype, handled
+        return None, None, None, None, None
+
+
+class VideoExtension(Extension):
+    """
+    Manage configuration options for the KBD markdown extension and attach 
+    patterns for each configured Markdown syntactical element.
+    """
+
+    def __init__(self, **kwargs):
+        # Define config options and defaults
+        # self.config = {
+        #     "Key": ["Default", "Description"],
+        # }
+        super().__init__(**kwargs)
+
+    def extendMarkdown(self, md):
+        md.inlinePatterns.register(VideoInlineProcessor(IMAGE_LINK_RE, md), 'video_link', 180)
+
+
+def makeExtension(**kwargs):
+    """
+    Return an instance of the KBD Python-Markdown extension.
+    This method enables the extension for use in MkDocs.
+    """
+    return VideoExtension(**kwargs)
