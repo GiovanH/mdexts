5a70429189c631af822a5fcf0640674cf3a8ad29
Gio <sethg@ipi.org>
2020-08-09 23:41:09 -0500

    Fix various HTML bugs

--- spoilerbox.py
+++ spoilerbox.py
@@ -70,8 +70,14 @@ class SpoilerblockPreprocessor(markdown.preprocessors.Preprocessor):
 
                 # These HTML blocks get shelved so that the inner content gets rendered as usual.
 
-                pre_html = self.md.htmlStash.store(pre_html)
-                post_html = self.md.htmlStash.store(post_html)
+                # Ok so this is actually the dumbest hack ever to work around a hideous bug
+                # The markdown processor usually doesn't process text inside HTML.
+                # The whole point of stashing the HTML part is to let the markdown processor
+                # process the file as if it were flat even though it's actually nested HTML.
+                # But the stash tag *itself* *does* get processed as text and wrapped in paragraph tags,
+                # so we have to perform an HTML injection attack on ourself here to unescape our content.
+                pre_html = self.md.htmlStash.store("</p>" + pre_html + "<p>")
+                post_html = self.md.htmlStash.store("</p>" + post_html + "<p>")
 
                 text = "\n".join([before, pre_html, content, post_html, after])
             else:
